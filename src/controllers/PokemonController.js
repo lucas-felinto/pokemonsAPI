64e8bae4924f4df842527d6f373d8382f4e40035
lucas-felinto <lucasfepinto@gmail.com>
2020-06-05 16:56:52 -0300

    feat crud

--- src/controllers/PokemonController.js
+++ src/controllers/PokemonController.js
@@ -1,12 +1,28 @@
 const Pokemon = require("../models/Pokemon")
 
 module.exports = {
+    async index (req, res) {
+        const pokemons = await Pokemon.findAll()
+
+        return res.status(200).json(pokemons)
+    },
+    async show (req, res) {
+        const { id } = req.params
+        const pokemon = await Pokemon.findByPk(id)
+        
+        if (!pokemon) {
+            return res.status(400).json({message: 'O pokemon não pode ser encontrado'})
+        }
+
+        return res.status(200).json(pokemon)
+    },
     async store (req, res) {
         const { tipo, treinador, nivel } = req.body
+        const acceptedPokemons = ["mewtwo", "charizard", "pikachu"]
 
-        // if (req.body.tipo != "charizard" || "mewtwo" || "pikachu") {
-        //     return res.status(400).json({message: 'O tipo não pode ser diferente de charizard, mewtwo ou pikachu'})
-        // }
+        if (!acceptedPokemons.find(pokemon => pokemon === tipo)) {
+            return res.status(400).json({message: "O tipo não pode ser diferente de charizard, mewtwo ou pikachu"})
+        }
 
         const pokemon = await Pokemon.create({
             tipo,
@@ -15,5 +31,19 @@ module.exports = {
         })
 
         return res.status(200).json(pokemon)
+    }, 
+    async put (req, res) {
+        const { id } = req.params
+        const { treinador } = req.body
+
+        await Pokemon.update({ treinador: treinador }, { where: { id } }) 
+
+        return res.status(204).json({ message: "Treinador atualizado"})
+    }, 
+    async delete (req, res) {
+        const { id } = req.params
+        await Pokemon.destroy({ where: { id } })
+
+        return res.status(204)
     }
 }
--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,10 @@ const PokemonController = require("./controllers/PokemonController")
 
 const routes = express.Router()
 
+routes.get("/pokemons", PokemonController.index)
+routes.get("/pokemons/:id", PokemonController.show)
 routes.post("/pokemons", PokemonController.store)
+routes.put("/pokemons/:id", PokemonController.put)
+routes.delete("/pokemons/:id", PokemonController.delete)
 
 module.exports = routes
